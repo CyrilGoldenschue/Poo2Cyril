8f954ffe7d0feade2504f47f93763539f727e7ad
Cyril <cyril.goldenschue@cpnv.ch>
2021-05-18 10:40:58 +0200

    Fin Ex 2

---
+++ Exercices/Queue/ArrayQueue.rb
@@ -0,0 +1,22 @@
+class ArrayQueue
+    def initialize
+      @data = []
+    end
+    
+    def enqueue(value)
+      @data.push(value)
+    end
+    
+    def dequeue
+      @data.shift
+    end
+    
+    def empty?
+      @data.empty?
+    end
+    
+    def size
+      @data.size
+    end
+  end
+  
