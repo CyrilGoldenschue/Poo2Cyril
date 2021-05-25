6a22ad8d5b39595fb6f55aef684436baa21f1c24
Cyril <cyril.goldenschue@cpnv.ch>
2021-05-25 08:23:31 +0200

    implémentation de l'insertion dans un enfant

--- Exercices/Arbre/main.rb
+++ Exercices/Arbre/main.rb
@@ -1,9 +1,11 @@
 require "./tree"
 
-#tree = WordSearchTree.new
+tree = WordSearchTree.new
 #p tree.hello
 
-functions_array = File.readlines(ARGV[0] || 'functions.txt', chomp: true)
+#functions_array = File.readlines(ARGV[0] || 'functions.txt', chomp: true)
 
-functions_tree = WordSearchTree.new
-functions_tree.fill_tree(functions_array)
+#functions_tree = WordSearchTree.new
+#functions_tree.fill_tree(functions_array)
+
+tree.insert("test")
--- Exercices/Arbre/tree.rb
+++ Exercices/Arbre/tree.rb
@@ -1,10 +1,43 @@
-require "./node"
+
 class WordSearchTree
+    class Node
+
+        def initialize(value, endWord)
+            @value = value, 
+            @children = Array.new,
+            @endWord = endWord
+            #@alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        end
+    
+        def add_child(new_node)
+            @children.push(new_node)
+        end
+    
+    end
 
     def initialize
-        @root = Node.new,
-        @alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-      end
+        @root = Node.new("ROOT", false)
+    end
+
+    def insert(word)
+        i = 0
+        node = ""
+        allLetters = word.chars
+        allLetters.each do |letter, index|
+            if(i == 0)
+                @root.add_child(node = Node.new(letter, false))    
+                  
+            elsif(index == allLetters.size-1)
+                @root.add_child(node = Node.new(letter, true)) 
+            else
+                @root.add_child(node = Node.new(letter, false))        
+            end
+            i += 1
+        end
+
+        p @root
+    end
+
 
     def add_children
         @root.add_child(Node.new)
